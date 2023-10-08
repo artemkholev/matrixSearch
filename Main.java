e5a4574972c45b6cf886fb3e1d6872aef4f1a5b2
Artem <artemkholev@yandex.ru>
2023-10-08 18:56:19 +0300

    add new data

--- Main.java
+++ Main.java
@@ -2,6 +2,18 @@ import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
+  public static String binarySearch(int start, int end, int row, int target, int[][] matrix) {
+    while(start <= end) {
+      int middle = (start + end) / 2;
+      if (matrix[row][middle] == target) {
+        return String.valueOf(row) + middle;
+      } else if (matrix[row][middle] < target) {
+        start = middle + 1;
+      } else if (matrix[row][middle] > target)
+        end = middle - 1;
+    }
+    return "";
+  }
   public static long measureTime(Runnable codeToMeasure) {
     long startTime = System.nanoTime();
     codeToMeasure.run();
@@ -28,36 +40,49 @@ public class Main {
     String answer = "";
     for (int i = 0; i < M && answer.isEmpty(); i++) {
       int first = 0, last = N - 1;
-      while(first <= last) {
-        int middle = (first + last) / 2;
-        if (matrix[i][middle] == target) {
-          answer = String.valueOf(i) + middle;
-          break;
-        } else if (matrix[i][middle] < target) {
-          first = middle + 1;
-        } else if (matrix[i][middle] > target)
-          last = middle - 1;
-      }
+      answer = binarySearch(first, last, i, target, matrix);
     }
 //    System.out.println(!answer.isEmpty() ? "Yes" : "False");
   }
 
-  long Third() {
-    long startTime = System.currentTimeMillis();
-    long endTime = System.currentTimeMillis();
-    return endTime - startTime;
+  static void third(int N, int M, int[][] matrix, int target) {
+    String answer = "";
+    int i = 1, j = N - 1;
+    while (i < M && matrix[i][j] < target) {
+      i *= 2;
+    }
+
+    int find_pos_start = i / 2, find_pos_end = Math.min(i, M - 1);
+    for (int k = find_pos_start; k <= find_pos_end; k++){
+      i = 1;
+      while (i < N && matrix[k][i] < target) {
+        i *= 2;
+      }
+      int first = i / 2, last = Math.min(i, N - 1);
+      answer = binarySearch(first, last, k, target, matrix);
+      if (!answer.isEmpty()) {
+//        System.out.println("Yes");
+        return;
+      }
+    }
+//    System.out.println("False");
   }
+
   public static void main(String[] args) {
     ArrayList<String> time = new ArrayList<String>();
 
     for (int k = 10; k <= Math.pow(2, 14); k += Math.pow(2, 6)) {
+//      int k = 10;
       final int M = k - k / 2 - k / 3,
                 N = k;
       int[][] matrix = new int[M][N];
-      int target = 2 * N + 1;
+//      int target = 2 * N + 1;
+      int target = 16 * N + 1;
+//      int target = 2 * N;
       for (int i = 0; i < M; i++) {
         for (int j = 0; j < N; j++) {
-          matrix[i][j] = (N / M * i + j) * 2;
+//          matrix[i][j] = (N / M * i + j) * 2;
+          matrix[i][j] = (N / M * i * j) * 2;
         }
       }
 
@@ -68,8 +93,10 @@ public class Main {
 //        System.out.println();
 //      }
 //      System.out.println(target);
+
 //      long process = measureTime(() -> { first(N, M, matrix, target); });
       long process = measureTime(() -> { second(N, M, matrix, target); });
+//      long process = measureTime(() -> { third(N, M, matrix, target); });
       time.add(N + " " + M + ", " + process + "\n");
     }
     System.out.println(time);
